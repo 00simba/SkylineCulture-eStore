813da77a49063a50b70ccdf5778e53696802eea6
00simba <sameer15557@gmail.com>
2022-11-27 23:37:06 -0500

    Testing HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import Website from './Website';
-import {BrowserRouter as Router}  from "react-router-dom";
+import {BrowserRouter as Router, HashRouter}  from "react-router-dom";
 
-ReactDOM.render(<Router><Website/></Router>,document.getElementById('root'));
+ReactDOM.render(<HashRouter><Website/></HashRouter>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

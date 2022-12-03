d6a95de051bd31c86deeac3924c90dc4d07d13be
00simba <sameer15557@gmail.com>
2022-12-02 21:43:42 -0500

    Added more products

--- client/src/Data/Data.js
+++ client/src/Data/Data.js
@@ -58,5 +58,76 @@ export default [
 
         "specs": "Size: 2 inches on longest side\n Material: Constructed of metal",
         "shipping": [shipping[0]]
+    },
+    {
+        "id" : 5,
+        "title" : "Turbo Keychain",
+        "img" : "turbo_keychain/turbo_main.png",
+        "price" : 9,
+        "url" : "turbo-keychain",
+        "description" : [
+            
+        "Featuring the record setting sports car dubbed 'Godzilla', the R32 GTR! Represent your passion in this automotive icon by supporting this keychain!",         
+        "Fully designed at SkylineCulture, and available ONLY at SkylineCulture for you to show off on your key set."],
+
+        "specs": "Size: 2 inches on longest side\n Material: Constructed of metal",
+        "shipping": [shipping[0]]
+    },
+    {
+        "id" : 6,
+        "title" : "NOS Keychain",
+        "img" : "nos_keychain/nos_main.png",
+        "price" : 9,
+        "url" : "nos-keychain",
+        "description" : [
+            
+        "Featuring the record setting sports car dubbed 'Godzilla', the R32 GTR! Represent your passion in this automotive icon by supporting this keychain!",         
+        "Fully designed at SkylineCulture, and available ONLY at SkylineCulture for you to show off on your key set."],
+
+        "specs": "Size: 2 inches on longest side\n Material: Constructed of metal",
+        "shipping": [shipping[0]]
+    },
+    {
+        "id" : 7,
+        "title" : "Gearbox Keychain",
+        "img" : "gearbox_keychain/gearbox_main.png",
+        "price" : 9,
+        "url" : "gearbox-keychain",
+        "description" : [
+            
+        "Featuring the record setting sports car dubbed 'Godzilla', the R32 GTR! Represent your passion in this automotive icon by supporting this keychain!",         
+        "Fully designed at SkylineCulture, and available ONLY at SkylineCulture for you to show off on your key set."],
+
+        "specs": "Size: 2 inches on longest side\n Material: Constructed of metal",
+        "shipping": [shipping[0]]
+    },
+    ,
+    {
+        "id" : 8,
+        "title" : "Suspension Keychain",
+        "img" : "suspension_keychain/suspension_main.png",
+        "price" : 9,
+        "url" : "suspension-keychain",
+        "description" : [
+            
+        "Featuring the record setting sports car dubbed 'Godzilla', the R32 GTR! Represent your passion in this automotive icon by supporting this keychain!",         
+        "Fully designed at SkylineCulture, and available ONLY at SkylineCulture for you to show off on your key set."],
+
+        "specs": "Size: 2 inches on longest side\n Material: Constructed of metal",
+        "shipping": [shipping[0]]
+    },
+    {
+        "id" : 9,
+        "title" : "TE37 Keychain",
+        "img" : "te37_keychain/te37_main.png",
+        "price" : 9,
+        "url" : "te37_keychain",
+        "description" : [
+            
+        "Featuring the record setting sports car dubbed 'Godzilla', the R32 GTR! Represent your passion in this automotive icon by supporting this keychain!",         
+        "Fully designed at SkylineCulture, and available ONLY at SkylineCulture for you to show off on your key set."],
+
+        "specs": "Size: 2 inches on longest side\n Material: Constructed of metal",
+        "shipping": [shipping[0]]
     } 
 ]

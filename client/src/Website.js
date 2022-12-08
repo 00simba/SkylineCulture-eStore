073db5aa4df85467158116d4650407ff76b2e6a6
00simba <sameer15557@gmail.com>
2022-12-08 11:17:24 -0500

    Changed redirect URL in Route

--- client/src/Website.js
+++ client/src/Website.js
@@ -108,7 +108,7 @@ export default function Website(){
                     <Route path='/cart' element={<CartPage changeId={id => setId(id)} setCartItems={setCartItems} cartItems={cartItems}/>}/>
                     <Route path={`/checkout/${id}`} element={<Checkout changeCountry={changeCountry} changeId={id => setId(id)} cartItems={cartItems}/>}></Route>
                     <Route path={`/payment/${id}`} cartItems={cartItems} element={<Payment setPromise={setPromise} setSecret={setSecret} country={country} changeId={id => setId(id)} cartItems={cartItems}/>}></Route>
-                    <Route path='/order-complete' element={<OrderComplete/>}/>
+                    <Route path={`/order-complete?payment_intent=${stripePromise}&payment_intent_client_secret=${clientSecret}&redirect_status=succeeded`} element={<OrderComplete/>}/>
                     <Route path="/*" element={<PageNotFound/>}/>
             </Routes>
             {![`/checkout/${id}`, `/payment/${id}`].includes(location.pathname) && <Footer/>}

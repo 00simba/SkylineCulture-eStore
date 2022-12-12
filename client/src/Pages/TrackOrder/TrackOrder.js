ef2f4bd3950fe95368aa295b6495e5f94282aeba
00simba <sameer15557@gmail.com>
2022-12-11 21:17:31 -0500

    Updated styling on Track Order page

--- client/src/Pages/TrackOrder/TrackOrder.js
+++ client/src/Pages/TrackOrder/TrackOrder.js
@@ -16,21 +16,38 @@ export default function TrackOrder(){
 
     return(
         <div className='trackingContainer'>
-            <h2>Track Order</h2>
-            <br/>
-            <form>
-                <input onChange={(e) => setID(e.target.value)} value={orderID} type='text' placeholder='Enter Order ID'></input>
-            </form>
-            <br/>
-            <button onClick={(e) => handleSubmit(e)}>Track Order</button>
-            <br/>
-            {shippingInfo && <p>Postage: {shippingInfo.postage_description}</p>}
-            <br/>
-            {shippingInfo && <p>Tracking History</p>}
-            <br/>
-            {shippingInfo && shippingInfo.tracking_events.map((object) => {
-                return <div>{object.created_at} - {object.title} - {object.location_description}</div>
-            })}
+            <div className='trackingDiv'>
+                <h2>Track Order</h2>
+                <form className='orderForm'>
+                    <input onChange={(e) => setID(e.target.value)} value={orderID} type='text' placeholder='Enter Shipment ID'></input>
+                </form>
+                <button onClick={(e) => handleSubmit(e)} className='trackButton'>Submit</button>
+                <div className='trackingInfo'>
+                    <div className='shipmentInfo'>
+                        <div className='infoRow'><span className='infoLabel'>Carrier:&nbsp;</span> {shippingInfo && shippingInfo.carrier.toUpperCase()}</div>
+                        <div className='infoRow'><span className='infoLabel'>Carrier Tracking Code:&nbsp;</span>{shippingInfo && shippingInfo.carrier_tracking_code}</div>
+                        <div className='infoRow'><span className='infoLabel'>Postage:&nbsp;</span>{shippingInfo && shippingInfo.postage_description}</div>
+                        <div className='infoRow'><span className='infoLabel'>Type:&nbsp;</span>{shippingInfo && shippingInfo.package_description}</div>
+                        <div className='infoRow'><span className='infoLabel'>Weight:&nbsp;</span>{shippingInfo && shippingInfo.weight_description}</div>
+                        <div className='infoRow'><span className='infoLabel'>Dimensions:&nbsp;</span>{shippingInfo && shippingInfo.size_description}</div>                    
+                    </div>
+                    <span className='infoLabel'>Latest Updates:</span>
+                    <div className='trackingHistory'>
+                            {shippingInfo && shippingInfo.tracking_events.map((object) => {
+                            var date = new Date( Date.parse(object.created_at)).toDateString().substring(4)
+                        if(date[4] === '0'){
+                            date = date.replace(date[4], '')
+                        }
+                            const time = new Date( Date.parse(object.created_at)).toLocaleTimeString().slice(0, -6) + ' ' + new Date( Date.parse(object.created_at)).toLocaleTimeString().slice(-2) 
+                            return <div>{date} {time} - {object.title} - {object.location_description}</div>
+                        })}
+                    </div>
+                </div>
+            </div>
+            <div className='shippingFAQ'>
+                <h2>Shipment Information</h2>
+                <p>All orders ship from Canada. Please allow 1-2 business days for fulfillment.</p>
+            </div>
         </div>
     )
 }

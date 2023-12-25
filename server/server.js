ab8c0ca6b31f725e79cd3c61f8d5b28f9d4eaec3
Sameer Qureshi <sameerqureshi@sameers-mbp.home>
2023-12-25 00:44:23 -0500

    Added parseFloat total

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.post('/config', (req, res) => {
 function calculateTotal(){
     var total = 0.00;
     (cart.items).forEach((itemObject) => {
-        var price = ((storeItems.get(parseInt(itemObject.productId))).price)*(parseInt(itemObject.productQuantity))
+        var price = parseFloat(((storeItems.get(parseInt(itemObject.productId))).price)*(parseInt(itemObject.productQuantity)))
         total += price 
     })
     if(total < 35.00){
